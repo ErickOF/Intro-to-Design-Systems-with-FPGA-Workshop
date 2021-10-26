ea5dad44b3a1fefd24985a42cd34dd146cd812d5
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2021-10-26 11:21:20 -0600

    [Add] - Generate Bitsteam

--- my-system/my-system.gen/sources_1/bd/system/hdl/system_wrapper.v
+++ my-system/my-system.gen/sources_1/bd/system/hdl/system_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
-//Date        : Mon Oct 25 22:37:19 2021
+//Date        : Tue Oct 26 11:11:03 2021
 //Host        : erickof-Inspiron-3576 running 64-bit Ubuntu 20.04.3 LTS
 //Command     : generate_target system_wrapper.bd
 //Design      : system_wrapper
