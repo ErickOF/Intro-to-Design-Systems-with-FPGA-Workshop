8300bbf7d6bead90f66d79a7fd8ad992a629add4
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2021-10-26 11:23:44 -0600

    [Add] - Copy of the system with the Bitstream generated

---
+++ my-system-workshop/my-system.gen/sources_1/bd/system/ip/system_rst_ps7_0_102M_0/system_rst_ps7_0_102M_0_stub.v
@@ -0,0 +1,31 @@
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
+// Date        : Tue Oct 26 09:59:47 2021
+// Host        : erickof-Inspiron-3576 running 64-bit Ubuntu 20.04.3 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/erickof/Documentos/Git/Intro-to-Design-Systems-with-FPGA-Workshop/my-system/my-system.gen/sources_1/bd/system/ip/system_rst_ps7_0_102M_0/system_rst_ps7_0_102M_0_stub.v
+// Design      : system_rst_ps7_0_102M_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "proc_sys_reset,Vivado 2020.2" *)
+module system_rst_ps7_0_102M_0(slowest_sync_clk, ext_reset_in, aux_reset_in, 
+  mb_debug_sys_rst, dcm_locked, mb_reset, bus_struct_reset, peripheral_reset, 
+  interconnect_aresetn, peripheral_aresetn)
+/* synthesis syn_black_box black_box_pad_pin="slowest_sync_clk,ext_reset_in,aux_reset_in,mb_debug_sys_rst,dcm_locked,mb_reset,bus_struct_reset[0:0],peripheral_reset[0:0],interconnect_aresetn[0:0],peripheral_aresetn[0:0]" */;
+  input slowest_sync_clk;
+  input ext_reset_in;
+  input aux_reset_in;
+  input mb_debug_sys_rst;
+  input dcm_locked;
+  output mb_reset;
+  output [0:0]bus_struct_reset;
+  output [0:0]peripheral_reset;
+  output [0:0]interconnect_aresetn;
+  output [0:0]peripheral_aresetn;
+endmodule
